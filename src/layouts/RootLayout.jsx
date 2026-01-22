7a7871181f5aa6d0ba5cba7ed5fcb45306dd9807
savecodes <codedevs7@gmail.com>
2026-01-22 11:59:18 +0600

    Implement ScrollToTop for route navigation

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../pages/Shared/Navbar/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../pages/Shared/Footer/Footer";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <div>
+      <ScrollToTop />
       <header className="sticky top-0 z-50">
         <Navbar />
       </header>
